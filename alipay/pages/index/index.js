8c4b884a16c0280c924f2b74fdcd3cd5fd6a5e2f
陶士涵 <630892807@qq.com>
2020-10-21 21:56:17 +0800

    新增加访客上线

--- alipay/pages/index/index.js
+++ alipay/pages/index/index.js
@@ -1,8 +1,10 @@
 Page({
   data: {
-    baseUrl:"https://gofly.sopans.com",
-    wsBaseUrl:"wss://gofly.sopans.com/ws_kefu?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVfdGltZSI6MTYwMzI1OTk2Niwia2VmdV9pZCI6MSwibmFtZSI6ImtlZnUyIiwicm9sZV9pZCI6MSwidHlwZSI6ImtlZnUifQ.RRA69WaopRRL4rtxetXRh85nvDhYWFnKeOUlKCdLSNw",
-    visitors:[]
+    //baseUrl:"https://gofly.sopans.com",
+    baseUrl:"http://127.0.0.1:8081",
+    wsBaseUrl:"ws://127.0.0.1:8081/ws_kefu?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVfdGltZSI6MTYwMzI1OTk2Niwia2VmdV9pZCI6MSwibmFtZSI6ImtlZnUyIiwicm9sZV9pZCI6MSwidHlwZSI6ImtlZnUifQ.RRA69WaopRRL4rtxetXRh85nvDhYWFnKeOUlKCdLSNw",
+    //wsBaseUrl:"wss://gofly.sopans.com/ws_kefu?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVfdGltZSI6MTYwMzI1OTk2Niwia2VmdV9pZCI6MSwibmFtZSI6ImtlZnUyIiwicm9sZV9pZCI6MSwidHlwZSI6ImtlZnUifQ.RRA69WaopRRL4rtxetXRh85nvDhYWFnKeOUlKCdLSNw",
+    visitors:[],
   },
   login(){
     my.alert({ title: 'You click reset' });
@@ -17,13 +19,55 @@ Page({
       my.alert({content: '连接已关闭！'});
     });
     my.onSocketOpen((res) => {
-					console.log("WebSocket 连接已打开1");
-		});
+          console.log("WebSocket 连接已打开");
+            let _this=this;
+            let mes = {}
+            mes.type = "ping";
+            mes.data = "";
+            setInterval(function () {
+                  my.sendSocketMessage({
+                      data: JSON.stringify(mes),
+                  });
+            },5000)
 
-    my.onSocketMessage((res) => {
-      var visitors=_this.data.visitors;
-      console.log('收到服务器内容 ：' + res.data)
     });
+    my.onSocketMessage((res) => {
+      var redata = JSON.parse(res.data);
+      switch (redata.type){
+          case "allUsers":
+              break;
+          case "userOnline":
+              _this.addOnlineUser(redata.data);
+              break;
+          case "userOffline":
+            
+              break;
+          case "notice":
+          
+              break;
+      }
+      //console.log('收到服务器内容 ：' + res.data)
+    });
+
+
+  },
+  addOnlineUser(retData){
+      var visitors=this.data.visitors;
+      var flag=false;
+      retData.last_message="新访客";
+      retData.name=retData.username;
+      for(let i=0;i<visitors.length;i++){
+          if(visitors[i].uid==retData.uid){
+              flag=true;
+          }
+      }
+     console.log(visitors,retData);
+      if(!flag){
+          visitors.unshift(retData);
+      }
+      this.setData({
+        visitors: visitors,
+      });
   },
   onLoad(){
     let _this=this;
