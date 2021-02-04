b11ef93e434b1180902a1a90b21f3c4b07ec4ba7
陶士涵 <630892807@qq.com>
2021-02-04 23:02:02 +0800

    增加列表展示

--- uniapp/GOFLY客服/pages/index/index.vue
+++ uniapp/GOFLY客服/pages/index/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<view class="content">
 		<uni-list>
-		    <uni-list-item title="列表左侧带略缩图" note="列表描述信息" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png"
-		     thumb-size="lg" rightText="右侧文字"></uni-list-item>
+		    <uni-list-item v-for="item in visitors" :title="item.username" :note="item.last_message" :thumb="baseUrl+item.avator"
+		     thumb-size="lg" ></uni-list-item>
 		</uni-list>
 	</view>
 
@@ -18,7 +18,7 @@
 				// baseUrl:"http://127.0.0.1:8081",
 				// wsBaseUrl:"ws://127.0.0.1:8081/ws_kefu",
 				visitors:[],
-				token:"",
+				token:".eyJjcmVhdGVfdGltZSI6MTYxMjQ0Mjc3Niwia2VmdV9pZCI6MSwibmFtZSI6ImtlZnUyIiwicm9sZV9pZCI6MSwidHlwZSI6ImtlZnUifQ.dUS3A9tMOnxCaWYT0xIOGpUOycoboSDuS0EvRyupUHc",
 				timer:null,
 				wsOpen:false,
 			}
@@ -31,7 +31,88 @@
 		onLoad() {
 		},
 		methods: {
+			//用户实时上下线
+			onlineIntime(){
+			  var _this=this;
+			  var timer=null;
+			  uni.connectSocket({
+			    url: this.wsBaseUrl+"?token="+this.token,
+			  });
+			  uni.onSocketClose((res) => {
+			    clearInterval(timer);
+			    console.log("WebSocket 连接断开");
+			  });
+			  uni.onSocketOpen((res) => {
+			        console.log("WebSocket 连接已打开");;
+			          let _this=this;
+			          let mes = {}
+			          mes.type = "ping";
+			          mes.data = "";
+			          timer=setInterval(function () {
+			                uni.sendSocketMessage({
+			                    data: JSON.stringify(mes),
+			                });
+			          },5000)
+			
+			  });
+			  uni.onSocketMessage((res) => {
+			    var redata = JSON.parse(res.data);
+			    switch (redata.type){
+			        case "allUsers":
+			            break;
+			        case "userOnline":
+			            _this.addOnlineUser(redata.data);
+			            break;
+			        case "userOffline":
+			           _this.removeOfflineUser(redata.data);
+			            break;
+			        case "notice":
+			            break;
+			        case "message":
+			          _this.recvMessage(redata.data);
+			        break;
+			    }
+			  });
+			},
 			getOnlineUser(){
+				let _this=this;
+				var baseUrl=this.baseUrl;
+				uni.request({
+					url: baseUrl+'/visitors_kefu_online?token='+_this.token,
+					method: 'GET',
+					success: function(res) {
+						console.log(res);
+						if(!res.data.result){
+							return;
+						}
+						_this.visitors=res.data.result;
+					},
+					fail: function(res) {
+					}
+				});
+			},
+			addOnlineUser(retData){
+			    var visitors=this.visitors;
+			    var flag=false;
+			    for(var i=0;i<visitors.length;i++){
+			        if(visitors[i].uid==retData.uid){
+			            flag=true;
+			        }
+			    }
+			    if(!flag){
+			        visitors.unshift(retData);
+			    }
+			    this.visitors= visitors;
+			},
+			  //处理当前在线用户列表
+			removeOfflineUser:function (retData) {
+			  var visitors=this.visitors;
+			    for(var i=0;i<visitors.length;i++){
+			        if(visitors[i].uid==retData.uid){
+			            visitors.splice(i,1);
+			        }
+			    }
+			    this.visitors=visitors;
 			},
 			checkAuth(){
 				var _this=this;
@@ -39,9 +120,12 @@
 					url: _this.baseUrl+'/userinfo?token='+_this.token,
 					method: 'GET',
 					success: function(res) {
-					},
-					complete: function(res) {
-				
+						var code=res.data.code;
+						if(code!=200){
+						  uni.navigateTo({ url: '/pages/index/login' });
+						}else{
+						   _this.onlineIntime();
+						}
 					}
 				});
 			},
---
+++ uniapp/GOFLY客服/pages/index/login.vue
@@ -0,0 +1,70 @@
+<template>
+    <view class="">
+        <uni-forms :value="formData" ref="form">
+            <uni-forms-item label="用户名" name="name">
+                <uni-easyinput type="text" v-model="formData.name" placeholder="请输入姓名" />
+            </uni-forms-item>
+			<uni-forms-item label="密码" name="password">
+			    <uni-easyinput type="text" v-model="formData.password" placeholder="请输入密码" />
+			</uni-forms-item>
+            <button @click="submitForm">Submit</button>
+        </uni-forms>
+    </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				formData:{
+					name:'',
+					password:''
+				},
+			}
+		},
+		methods: {
+			submitForm(form) {
+				// 手动提交表单
+				this.$refs.form.submit().then((res)=>{
+					console.log('表单返回得值：', res)
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.uni-input-border,
+	.uni-textarea-border {
+		flex: 1;
+		font-size: 14px;
+		color: #666;
+		border: 1px #e5e5e5 solid;
+		border-radius: 5px;
+		/* #ifndef APP-NVUE */
+		box-sizing: border-box;
+		/* #endif */
+	}
+
+	.uni-input-border {
+		padding: 0 10px;
+		height: 35px;
+	}
+
+	.uni-textarea-border {
+		padding: 10px;
+		height: 80px;
+	}
+
+	.label-box {
+		margin-right: 10px;
+	}
+
+	.transform-scale {
+		transform: scale(0.7);
+	}
+
+	.button {
+		margin: 5px 10px;
+	}
+</style>
